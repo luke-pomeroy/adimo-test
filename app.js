a0181d63f3b0547203ace644f21b6a74733fe59b
Luke Pomeroy <lukepomeroy@hotmail.co.uk>
2023-02-19 22:24:58 +0000

    added calls to functions in app.js

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-const Product = require('/class/product')
-const axiosScraper = require('./scrape.js')
+const axiosScraper = require('./scrape')
+const Product = require('./class/product')
+
+let url = ''
+
+axiosScraper(url).then(async result => {
+    if (result.error) {
+        return console.log(result.error)
+    }
+
+    if (result.products.length > 0) {
+        let isoDate = new Date().toISOString()
+        let exportData = {
+            url: url,
+            dateRetrieved: isoDate,
+            totalProducts: result.products.length,
+            averagePrice: Product.averageCurrentPrice(result.products),
+            products: result.products
+        }
+        let fileResult = await Product.saveJsonFile(exportData, 'output', isoDate + '.json')
+        console.log(fileResult)
+    }
+
+    return 'Something else went wrong!'
+})
