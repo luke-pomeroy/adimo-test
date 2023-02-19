d9e01631f5d85d88990449a4d7d86730680c5ceb
Luke Pomeroy <lukepomeroy@hotmail.co.uk>
2023-02-19 18:17:07 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+axios.get('https://cdn.adimo.co/clients/Adimo/test/index.html')
+    .then(function(response) {
+        // HTML is inside response.data
+        let $ = cheerio.load(response.data)
+
+        $('div.item').each(function(i, elem){
+
+        })
+    })
+    .catch(function(error) {
+        //Print error if any occured
+        console.error('Error!: ', error.message)
+    })
---
+++ class/product.js
@@ -0,0 +1,20 @@
+class Product {
+
+    constructor(title, imageUrl, currentPrice, previousPrice) {
+
+    }
+
+    getDiscount() {
+
+    }
+
+    static averageCurrentPrice(products) {
+
+    }
+
+    static saveJsonFile(productData, filePath, fileName) {
+
+    }
+}
+
+module.exports = Product
