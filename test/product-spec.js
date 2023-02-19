777c8e6d3478dc9151b2cce6301c47d14477a15b
Luke Pomeroy <lukepomeroy@hotmail.co.uk>
2023-02-19 19:14:49 +0000

    amended test cases to reflect previousPrice is null, not £0.00

--- test/product-spec.js
+++ test/product-spec.js
@@ -2,51 +2,53 @@ const { expect } = require('chai');
 const Product = require('../class/product.js')
 
 describe ('Product class', function () {
-    let product1, product2, product3, products, average
+    let product1, product2, product3, products, averageProductPrice
 
     beforeEach(function() {
         // Product properties: title, imageUrl, currentPrice, previousPrice, discountAmount
         product1 = new Product(
-            "Cashel Blue",
-            "https://cdn.adimo.co/clients/Adimo/test/images/cashelblue.jpg",
-            "£5.43",
-            "£0.00"
+            'Cashel Blue',
+            'https://cdn.adimo.co/clients/Adimo/test/images/cashelblue.jpg',
+            '£5.43',
+            ''
         )
         product2 = new Product(
-            "Dunshyre Blue",
-            "https://cdn.adimo.co/clients/Adimo/test/images/dunshyreblue.jpg",
-            "£8.00",
-            "£0.00"
+            'Dunshyre Blue',
+            'https://cdn.adimo.co/clients/Adimo/test/images/dunshyreblue.jpg',
+            '£8.00',
+            ''
         )
         product3 = new Product(
-            "Dunshyre Blue",
-            "https://cdn.adimo.co/clients/Adimo/test/images/evesselection.jpg",
-            "£45.00",
-            "£50.00"
+            'Dunshyre Blue',
+            'https://cdn.adimo.co/clients/Adimo/test/images/evesselection.jpg',
+            '£45.00',
+            '£50.00'
         )
         products = [product1, product2, product3]
+        averageProductPrice = Product.averageCurrentPrice(products)
+        console.log(products)
     })
 
 
     describe('Product constructor', () => {
         it('should have set the title property', () => {
             expect(product1).to.have.property('title')
-            expect(product1.title).to.eql("Cashel Blue")
+            expect(product1.title).to.eql('Cashel Blue')
         })
 
         it('should have set the imageUrl property', () => {
             expect(product1).to.have.property('imageUrl')
-            expect(product1.imageUrl).to.eql("https://cdn.adimo.co/clients/Adimo/test/images/cashelblue.jpg")
+            expect(product1.imageUrl).to.eql('https://cdn.adimo.co/clients/Adimo/test/images/cashelblue.jpg')
         })
 
         it('should have set the currentPrice property', () => {
             expect(product1).to.have.property('currentPrice')
-            expect(product1.currentPrice).to.eql("£5.43")
+            expect(product1.currentPrice).to.eql('£5.43')
         })
 
         it('should have set the previousPrice property', () => {
             expect(product1).to.have.property('previousPrice')
-            expect(product1.previousPrice).to.eql("£0.00")
+            expect(product1.previousPrice).to.eql('')
         })
 
         it('should have set the discountAmount property', () => {
@@ -54,21 +56,20 @@ describe ('Product class', function () {
         })
     })
 
-    describe('getDiscount instance method', () => {
-        it('should set discountAmount to £0.00 if previousPrice == £0.00', () => {
-            expect(product1.discountAmount).to.eql("£0.00")
+    describe('setDiscount instance method', () => {
+        it('should set discountAmount to £0.00 if !previousPrice', () => {
+            expect(product1.discountAmount).to.eql('£0.00')
         })
 
-        it('should set discountAmount correctly if previousPrice != £0.00', () => {
-            expect(product3.discountAmount).to.eql("£5.00")
+        it('should set discountAmount correctly if previousPrice', () => {
+            expect(product3.discountAmount).to.eql('£5.00')
         })
     })
 
     describe('averageCurrentPrice static method', () => {
-        let average = Product.averageCurrentPrice(products)
         // Fractions of pence should be rounded to the nearest penny, e.g. 19.47666667 => £19.48
         it('should have correctly calculated the average current price of all products', () => {
-            expect(average).to.eql("£19.48")
+            expect(averageProductPrice).to.eql('£19.48')
         })
     })
 
@@ -77,19 +78,19 @@ describe ('Product class', function () {
         it('should return an error if productData contains no products', () => {
             let productData = []
             expect(Product.saveJsonFile(productData, '../output', 'ExampleFileName.json'))
-            .to.eql("Error!: Could not save to file, no product data.")
+            .to.eql('Error!: Could not save to file, no product data.')
         })
 
         it('should return an error if a filePath is not provided or is null', () => {
             let productData = products
             expect(Product.saveJsonFile(productData, '', 'ExampleFileName.json'))
-            .to.eql("Error!: Could not save to file, no product data.")
+            .to.eql('Error!: Could not save to file, no filePath provided.')
         })
 
         it('should return an error if a fileName is not provided or is null', () => {
             let productData = products
             expect(Product.saveJsonFile(productData, '../output', ''))
-            .to.eql("Error!: Could not save to file, no product data.")
+            .to.eql('Error!: Could not save to file, no fileName provided.')
         })
     })
 
