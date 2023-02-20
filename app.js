88c9f00e8cde01faca2900fbd1c03eeceb6ccb78
Luke Pomeroy <lukepomeroy@hotmail.co.uk>
2023-02-20 22:43:21 +0000

    added readline questions for user input

--- app.js
+++ app.js
@@ -1,15 +1,60 @@
+const readline = require('node:readline')
 const axiosScraper = require('./axiosScrape')
+const puppeteerScraper = require('./puppeteerScrape')
 const Product = require('./class/product')
 
-let url = 'https://cdn.adimo.co/clients/Adimo/test/index.html'
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-axiosScraper(url).then(async result => {
+const printHelp = () => {
+    console.log('Selection options:')
+    console.log('"1" to get cheeses from the adimo test site')
+    console.log('"2" to fetch results from thewhiskyexchange.com')
+}
+
+const selectOption = () => {
+    printHelp()
+    rl.question('Which option would you like to pick? ', getSelection)
+}
+
+const getSelection = (selection) => {
+    if (!selection || !(['1', '2'].includes(selection))) {
+        console.log('\nPlease enter a valid option!\n')
+        selectOption()
+    } else {
+        if (selection === '1') {
+            rl.close()
+            let url = 'https://cdn.adimo.co/clients/Adimo/test/index.html'
+            return fetchResults(url, axiosScraper)
+        }
+        rl.question('Enter your search term (or just enter "cider"): ', validateSearchTerm)
+    }
+}
+
+const validateSearchTerm = (searchTerm) => {
+    if (!searchTerm) {
+        console.log('\nA search term must be entered!\n')
+        return selectOption()
+    }
+    rl.close()
+    console.log('Fetching results...')
+    let url = `https://www.thewhiskyexchange.com/search?q=${searchTerm}`
+    return fetchResults(url, puppeteerScraper)
+}
+
+const fetchResults = async (url, cb) => {
+    const result = await cb(url)
+
     if (result.error) {
-        return console.log(result.error)
+        console.log(result.error)
+        return
     }
 
     if (result.products.length > 0) {
         let isoDate = new Date().toISOString()
+
         let exportData = {
             url: url,
             dateRetrieved: isoDate,
@@ -17,11 +62,13 @@ axiosScraper(url).then(async result => {
             averagePrice: Product.averageCurrentPrice(result.products),
             products: result.products
         }
+
         let fileResult = await Product.saveJsonFile(exportData, 'output', isoDate + '.json')
         console.log(fileResult)
+        return
     } else {
-        return 'No products found on page!'
+        console.log('No products found on page!')
+        return
     }
-
-    return 'Something else went wrong!'
-})
+}
+selectOption()
